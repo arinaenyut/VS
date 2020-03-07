cbf33d42280d0096bf5956e70db9fcd5c5f5748b
arinaenyut <arina.enyutina@mail.ru>
2020-03-07 20:20:20 +0700

    1

---
+++ huf.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+
+class Node{
+	public:
+        	int fq;//частота
+                char sym;//символ
+     		Node *left, *right;  
+    		Node(){
+    			left = NULL;
+    			right = NULL;
+    		} 
+ 		Node(Node *L, Node *R){ 
+ 			left =  L;
+                        right = R;
+                        sym = L->sym + R->sym;
+                }
+};
+
+
+int main (){
+	ifstream file ("text.txt", ios::binary);
+	map <char,int> letters;
+	char s;
+	while (!file.eof()){
+		s = file.get();
+		letters[s]++;}
+		
+	
+    
+
+
+return 0;}
